4bf37924f77c28d615e6121f080406ee59236ada
waterflow80 <thessalonikaathena@outlook.com>
2023-07-13 23:09:40 +0100

    first commit

---
+++ src/main/java/com/example/springbatchexample/SprintBatchExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatchexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBatchExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintBatchExampleApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/springbatchexample/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.example.springbatchexample.controller;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private Job job;
+
+    @GetMapping("/start")
+    public String sendBatch() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        Map<String, JobParameter> maps = new HashMap<>();
+        maps.put("timestamp", new JobParameter(System.currentTimeMillis()));
+        JobParameters parameters = new JobParameters(maps);
+        JobExecution jobExecution = jobLauncher.run(job, parameters);
+        return "Spring Batch job " + jobExecution.getStatus();
+    }
+}
